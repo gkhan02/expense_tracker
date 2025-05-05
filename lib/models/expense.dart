a671b982d005cf1450f607070541f214eaea8fc0
gulnuma khan <gkhan02@nyit.edu>
2025-05-04 22:35:10 -0400

    added bucket list

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -1,20 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 import 'package:intl/intl.dart';
+
 const uuid = Uuid();
 
 final formatter = DateFormat.yMd();
 
-enum Category {food, travel, leisure, work}
+enum Category { food, travel, leisure, work }
 
 const categoryIcons = {
   Category.food: Icons.lunch_dining,
   Category.travel: Icons.flight_takeoff,
   Category.leisure: Icons.movie,
-  Category.work: Icons.work
+  Category.work: Icons.work,
 };
 
-class Expense{
+class Expense {
   Expense({
     required this.title,
     required this.amount,
@@ -25,9 +26,30 @@ class Expense{
   final String title;
   final double amount;
   final DateTime date;
-  final Category category; // leisure Expense(category : 'leisure') 
+  final Category category; // leisure Expense(category : 'leisure')
 
-  String get formattedDate{
+  String get formattedDate {
     return formatter.format(date);
   }
+}
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+    : expenses =
+          allExpenses.where((expense) => expense.category == category).toList();
 }
